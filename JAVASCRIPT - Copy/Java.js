95e566fa54f50aaad6d5798e10f5de00d04a09af
Hamid455 <110316359+Hamid455@users.noreply.github.com>
2022-08-29 13:55:27 +0500

    Add files via upload

---
+++ JAVASCRIPT - Copy/Java.js
@@ -0,0 +1,38 @@
+
+function plus(){
+  let a = parseInt(document.getElementById('number').value) 
+      b = parseInt(document.getElementById('numbers').value)
+      document.getElementById('result').innerHTML = a+b 
+} 
+
+
+
+function minus(){
+  let a = parseInt(document.getElementById('number').value) 
+      b = parseInt(document.getElementById('numbers').value)
+      document.getElementById('result').innerHTML = a-b 
+} 
+
+function multiply(){
+  let a = parseInt(document.getElementById('number').value) 
+      b = parseInt(document.getElementById('numbers').value)
+      document.getElementById('result').innerHTML = a*b 
+} 
+
+function divide(){
+  let a = parseInt(document.getElementById('number').value) 
+      b = parseInt(document.getElementById('numbers').value)
+      document.getElementById('result').innerHTML = a/b 
+} 
+
+
+
+
+
+
+
+
+
+
+
+
